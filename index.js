644c6f10f9673802d4e15bd93a8ecd0bfef9d607
Vladyslav Simak <simakv21@gmail.com>
2024-09-09 18:09:27 +0300

    fix details

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 // index.js
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { API_TOKEN } = process.env;
+const { API_TOKEN, PORT } = process.env;
 
 const bot = new TelegramBot(API_TOKEN, { polling: true });
 const app = express();
-const PORT = 1111;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('public'));
-
-// Маршрут для веб-додатку
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
 
 // Обробка команди /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const webAppUrl = 'http://localhost:3000/'; // Замініть на URL вашого веб-додатку
+    const webAppUrl = 'http://localhost:1111/'; // Замініть на URL вашого веб-додатку
 
-    bot.sendMessage(chatId, 'Ласкаво просимо до Tap Bucket! Натисніть на кнопку нижче, щоб запустити гру.', {
+    bot.sendMessage(chatId, `${msg.chat.username}! Ласкаво просимо до Tap Bucket! Натисніть на кнопку нижче, щоб запустити гру.`, {
         reply_markup: {
             inline_keyboard: [
                 [{ text: 'Запустити гру', web_app: { url: webAppUrl } }]
